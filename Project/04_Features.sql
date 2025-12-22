943b000940103864fc643a1196e064fc53b6d8a8
Kallol Das Kushol <kallold70@gmail.com>
2025-12-23 01:43:24 +0600

    normal

--- Project/04_Features.sql
+++ Project/04_Features.sql
@@ -2,6 +2,7 @@
 SELECT t.train_name,
        s1.station_name AS Source_Station,
        s2.station_name AS Destination_Station
+    -- need to add the time details also
     FROM Train t
 JOIN Train_Schedule ts ON t.train_id = ts.train_id
 JOIN Station s1 ON ts.source_station = s1.station_id
@@ -9,7 +10,7 @@ JOIN Station s2 ON ts.destination_station = s2.station_id;
 
 -- 2. Find trains that start from a specific city (subquery)
 SELECT train_name
-FROM Train
+    FROM Train
 WHERE train_id IN (
     SELECT train_id
     FROM Train_Schedule
@@ -19,12 +20,13 @@ WHERE train_id IN (
         WHERE UPPER(city) = UPPER(:city_name)
     )
 );
- -- To clear the variable
+
+-- To clear the variable
 UNDEFINE city_name;
 
 -- 3. Passengers booked for a specific train (train_id = 101)
 SELECT name
-FROM Passenger
+    FROM Passenger
 WHERE passenger_id IN (
     SELECT passenger_id
     FROM Ticket
@@ -34,9 +36,10 @@ WHERE passenger_id IN (
 -- 4. Display train schedule between a time range
 
 SELECT *
-FROM Ticket
-WHERE journey_date >= TO_DATE(:start_date, 'YYYY-MM-DD')
+    FROM Ticket --ticket table has journey_date column
+WHERE journey_date >=  TO_DATE(:start_date, 'YYYY-MM-DD')
   AND journey_date <=  TO_DATE(:end_date,   'YYYY-MM-DD')
+  AND booking_status LIKE 'CONFIRMED'
 ORDER BY journey_date;
 
 -- To clear the variables
--- Project/05_Views_Triggers.sql
+++ Project/05_Views_Triggers.sql
@@ -14,9 +14,11 @@ LEFT JOIN Ticket t
 GROUP BY tr.train_id, tr.train_name, tr.total_seats;
 /
 
+-- Testing the view
 SELECT * FROM view_train_occupancy;
 
 -- Shows total revenue per journey date.
+
 CREATE OR REPLACE VIEW view_daily_revenue AS
 SELECT
     t.journey_date,
@@ -28,5 +30,29 @@ WHERE p.payment_status = 'PAID'
 GROUP BY t.journey_date;
 /
 
+-- Testing the view
 SELECT * FROM view_daily_revenue;
 
+
+-- Trigger to auto-cancel ticket if payment fails.
+
+CREATE OR REPLACE TRIGGER trg_cancel_on_payment_fail
+AFTER UPDATE OF payment_status ON Payment
+FOR EACH ROW
+BEGIN
+    IF :NEW.payment_status = 'FAILED' THEN
+        UPDATE Ticket
+        SET booking_status = 'CANCELLED'
+        WHERE ticket_id = :NEW.ticket_id;
+    END IF;
+END;
+/
+-- Testing the trigger
+-- Assume we have a payment with payment_id = 1
+UPDATE Payment
+SET payment_status = 'FAILED'
+WHERE payment_id = 1;  
+-- Check the corresponding ticket's booking_status
+SELECT booking_status
+FROM Ticket
+WHERE ticket_id = (SELECT ticket_id FROM Payment WHERE payment_id = 1);
